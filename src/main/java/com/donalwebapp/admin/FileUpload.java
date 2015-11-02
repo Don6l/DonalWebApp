f6730bc2d30a6a148a9fb10c972d8cc53b7f78c7
DonalMc <donalmc@ubuntu.ubuntu-domain>
2015-11-02 21:13:27 +0000

    Testing uploader

--- src/main/java/com/donalwebapp/admin/FileUpload.java
+++ src/main/java/com/donalwebapp/admin/FileUpload.java
@@ -1,14 +1,30 @@
 package com.donalwebapp.admin;
 
-import javax.ejb.LocalBean;
-import javax.ejb.Stateless;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 
-@Stateless
-@LocalBean
 public class FileUpload {
 
-	private String fileName;
+	private Workbook uploadDocument;
 	
+	public FileUpload(final String filePath) {
+		InputStream is;
+		try {
+			is = new FileInputStream(filePath);
+			uploadDocument = new XSSFWorkbook(is);
+			System.out.println("It's doing something, fileName="+filePath);
+		} catch (IOException ioException) {
+			throw new RuntimeException(ioException);
+		}
+	}
+	
+	public Workbook getUploadDocument() {
+		return uploadDocument;
+	}
 	
 }
---
+++ src/main/java/com/donalwebapp/admin/FileUploadWS.java
@@ -0,0 +1,30 @@
+package com.donalwebapp.admin;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.apache.poi.ss.usermodel.Workbook;
+
+@Path("/upload")
+@Stateless
+@LocalBean
+public class FileUploadWS {
+
+	private Workbook uploadDoc;
+	
+	@GET
+	@Path("/file/{path}")
+	@Produces({MediaType.APPLICATION_JSON})
+	public Response uploadData(@PathParam("path") final String path) {
+		final String parsedPath = path.replaceAll("&fs&", "/");
+		System.out.println("HERE: "+parsedPath);
+		uploadDoc = new FileUpload(parsedPath).getUploadDocument();
+		return Response.status(200).entity(uploadDoc.getSheetName(0)).build();
+	}
+}
