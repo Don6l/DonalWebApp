225b21489cf53f7ed1e1fc287bc8664b7b47ab60
DonalMc <donalmc@ubuntu.ubuntu-domain>
2015-11-02 19:54:37 +0000

    Fixing git issues

---
+++ src/main/java/com/donalwebapp/admin/FileUpload.java
@@ -0,0 +1,14 @@
+package com.donalwebapp.admin;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+
+@Stateless
+@LocalBean
+public class FileUpload {
+
+	private String fileName;
+	
+	
+}
