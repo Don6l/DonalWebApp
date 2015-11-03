9acd488e46ec51f680a90f41b9cbb96bc159a212
DonalMc <donalmc@ubuntu.ubuntu-domain>
2015-11-03 00:11:37 +0000

    Added setUp.sql file and changed PersistentObjectCreator to include most of the feilds

--- src/main/java/com/donalwebapp/reader/PersistentObjectCreator.java
+++ src/main/java/com/donalwebapp/reader/PersistentObjectCreator.java
@@ -15,9 +15,14 @@ public class PersistentObjectCreator {
 		WorldWideWebData wwwData = null;
 		try{
 			wwwData = new WorldWideWebData();
-			wwwData.setTest_id(Map.get("test_id");
-			
-			
+			wwwData.setTest_id(Integer.parseInt(map.get("test_id")));
+			//wwwData.setTest_date());
+			wwwData.setClient_ip_id(Integer.parseInt(map.get("client_ip_id")));
+			wwwData.setDownload_kbps(Integer.parseInt("download_kbps"));
+			wwwData.setUpload_kbps(Integer.parseInt("upload_kbps"));
+			wwwData.setLatency(Integer.parseInt("latency"));
+			wwwData.setServer_name(map.get("latency"));
+			wwwData.setServer_country(map.get("server_country"));
 		} catch(ParseException e){
 			e.printStackTrace();
 		}
